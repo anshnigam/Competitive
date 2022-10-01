78250383f89cf8cb1c12644eb6f299afd49f5c87
anshnigam <103136434+anshnigam@users.noreply.github.com>
2022-10-01 22:48:14 +0530

    CodeChef - Average Permutation - AVGPERM
    Link to question: https://www.codechef.com/submit/AVGPERM

---
+++ avgperm.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		while(t-- > 0){
+		    int n = Integer.parseInt(br.readLine());
+            if(n%2 == 0){
+                for(int i=n;i>=2;i-=2)
+                System.out.print(i+" ");
+                for(int i=1;i<n;i+=2)
+                System.out.print(i+" ");
+            }
+            else{
+                for(int i=n;i>=1;i=i-2)
+                System.out.print(i+" ");
+                for(int i=2;i<n;i=i+2)
+                System.out.print(i+" ");
+            }
+            System.out.println();
+		}
+	}
+}
