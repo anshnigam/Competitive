1a54a53502c67980d231bdf77a724bcc47aa85fb
anshnigam <103136434+anshnigam@users.noreply.github.com>
2022-10-21 18:06:05 +0530

    CodeChef - Single Operation Part 1 - SINGLEOP1
    Link to question: https://www.codechef.com/submit/SINGLEOP1

---
+++ singleop1.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static void solve(int n,String str){
+        int idx = -1;
+        for(int i=0;i<n;i++){
+            if(str.charAt(i) == '0'){
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1){
+            idx = n;
+        }
+        System.out.println(idx);
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		while(t-- > 0){
+		    int n = Integer.parseInt(br.readLine());
+            String s = br.readLine();
+            solve(n,s);
+		}
+	}
+}
