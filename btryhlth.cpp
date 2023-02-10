5b1d2d0a9a01bd05d611e5c33fc6b282ee0443d7
anshnigam <103136434+anshnigam@users.noreply.github.com>
2023-02-10 21:21:14 +0530

    CodeChef - battery health

---
+++ btryhlth.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x;
+    cin>>x;
+    if(x >=80)  cout<<"yes"<<endl;
+    else    cout<<"no"<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
